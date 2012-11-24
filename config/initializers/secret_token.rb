5e6b50d692b4dfc207c3e2825c74aa5276d0b744
Dmitry Shilin <dmitryshilin@agileorange.ru>
2012-11-24 13:59:43 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogAgileorange::Application.config.secret_token = 'b6c411aa6d4d2b6200f096ad3d1a89506e74990dc0bb2cf8dd7367b1cda2ff8c4d59ae7ef5d9a9da719733c5eda2a7420a18521b043b0fb34d111824d3dc920b'
